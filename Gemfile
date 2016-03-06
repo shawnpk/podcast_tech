4e0bbd493fb10aa6f081c245e0432dd85db3f471
Shawn Kearney <shawnpk@ineptsoftware.com>
2016-03-05 21:55:32 -0800

    Add aws-sdk gem and config to production.rb for Amazon S3 storage

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'devise', '~> 3.5', '>= 3.5.6'
 gem 'paperclip', '~> 4.3', '>= 4.3.5'
+gem 'aws-sdk', '~> 2.2', '>= 2.2.24'
 
 gem 'sdoc', '~> 0.4.0', group: :doc
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,14 @@ Rails.application.configure do
 
   # For devise
   config.action_mailer.default_url_options = { host: 'ies-podcast-tech.herokuapp.com' }
+
+  # Add images from the Paperclip gem to Amazon S3 for Heroku
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
